3ebdd3f58c24e63e65e832b744e0e263928863cd
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-29 23:17:35 -0600

    initiated html and app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
