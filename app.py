f1d700519bdc4102471de778aab5212a4e9d68be
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-12-01 17:03:15 -0600

    added the register route and login route logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
+from forms import AddUserForm, LoginForm
 
 app = Flask(__name__)
 
@@ -16,23 +17,46 @@ connect_db(app)
 
 @app.route("/")
 def root():
-    return render_template("")
+    return redirect("/register")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def new_user():
-    if user:
+
+    form = AddUserForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        pwd = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User.register(username, pwd, email, first_name, last_name)
+
+        db.session.add(new_user)
+        db.session.commit()
         return redirect("/secret")
     else:
-        return redirect("/")
+        return render_template("register.html", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login_user():
-    if user:
-        return redirect("/secret")
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        pwd = form.password.data
+        user = User.authenticate(username, pwd)
+        if user:
+            session["user_id"] = user.id
+            return redirect("/secret")
+        else:
+            form.username.errors = ["Incorrect Username/Password"]
     else:
-        return redirect("/")
+        return render_template("login.html", form=form)
 
 
 @app.route("/secret")
