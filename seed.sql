33a5e49c557c248f5726440cb36b76d87cecf0bd
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-12-06 16:30:30 -0600

    changed the pwd to password so it matches the models class it references

--- seed.sql
+++ seed.sql
@@ -5,7 +5,7 @@ CREATE DATABASE feedback;
 \c feedback
 CREATE TABLE users (
     username varchar(20) PRIMARY KEY,
-    pwd text NOT NULL,
+    password text NOT NULL,
     email varchar(50) NOT NULL UNIQUE,
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL
